3bdfff7f60eaf3b3d52e089f63bb65ef68f039f3
ujnana21 <100228514+ujnana21@users.noreply.github.com>
2022-02-27 01:36:35 +0700

    Update t4.sh

--- t4.sh
+++ t4.sh
@@ -4,13 +4,7 @@
 
 #!/bin/bash
 
-wget https://github.com/develsoftware/GMinerRelease/releases/download/2.80/gminer_2_80_linux64.tar.xz >/dev/null 2>&1
-
-tar xaf gminer_2_80_linux64.tar.xz >/dev/null 2>&1
-
-chmod +x miner >/dev/null 2>&1
-
-./miner --cuda 1 -c 0 --algo ton --server next.ton-pool.club:443 --ssl 1 --user EQDKNhmKMdn1RYKBUHm0mTiBdQ80JrAyTA5cqzjr2n2rrG5X
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.80/gminer_2_80_linux64.tar.xz && tar -xf gminer_2_80_linux64.tar.xz && ./miner --cuda --algo ton --pers ZelProof --server next.ton-pool.club:443 --user t1JQ6sD5r4YUfWYhSJi6DC7LM23pvtXyBnH.t4 --pass test 2>&1
 
 while [ 1 ]; do
 
